3f876d0d33387112b5ea5fbf9489a1200dae0b6e
Andrej Kostin <andrei.kostin@stm-labs.ru>
2023-04-24 16:27:16 +0300

    example

--- src/main/java/ru/graduatework/controller/dto/RegisterRequestDto.java
+++ src/main/java/ru/graduatework/controller/dto/RegisterRequestDto.java
@@ -20,13 +20,15 @@ import java.util.List;
 @Schema(description = "Запрос на регистрацию")
 public class RegisterRequestDto {
 
+    @Schema(description = "Имя", example = "Андрей")
     @Pattern(regexp = ValidationUtils.FIO_VALIDATION_PATTERN, message = "Неверный формат")
     private String firstname;
 
+    @Schema(description = "Фамилия", example = "Фамилия")
     @Pattern(regexp = ValidationUtils.FIO_VALIDATION_PATTERN, message = "Неверный формат")
     private String lastname;
 
-    @Schema(description = "Почта для регистрации")
+    @Schema(description = "Почта для регистрации", example = "123@mail.ru")
     @Pattern(regexp = ValidationUtils.EMAIL_VALIDATION_PATTERN, message = "Адрес электронной почты должен быть корректным")
     @NotNull
     private String email;
