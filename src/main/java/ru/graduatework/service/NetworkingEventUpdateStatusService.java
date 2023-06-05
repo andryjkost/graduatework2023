849cbfc1eefbb9025838d0ae00ded8f8e78eddae
Andrej Kostin <andrei.kostin@stm-labs.ru>
2023-06-05 04:04:15 +0300

    scheduler fix

--- src/main/java/ru/graduatework/service/NetworkingEventUpdateStatusService.java
+++ src/main/java/ru/graduatework/service/NetworkingEventUpdateStatusService.java
@@ -2,15 +2,27 @@ package ru.graduatework.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import ru.graduatework.jdbc.PostgresOperatingDb;
+import ru.graduatework.repository.NetworkingEventRepository;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class NetworkingEventUpdateStatusService {
 
+    @Value("${networking-event.duration}")
+    private String durationOfEventDefault;
+
+    private final PostgresOperatingDb db;
+    private final NetworkingEventRepository networkingEventRepository;
+
     public Mono<Void> updateStatus(){
-        return Mono.empty();
+        return db.transactionalExecAsync(ctx->{
+           var list = networkingEventRepository.getList(ctx);
+           return null;
+        });
     }
 }
